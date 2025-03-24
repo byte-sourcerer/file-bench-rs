ec31107d184ea439d59405b119202e612ebaabd4
Kermit <chenjiawei1@xiaohongshu.com>
2025-03-24 21:10:03 +0800

    add `generate_random_file_offset`

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,49 @@
 use parse_size::parse_size;
-use rand::{RngCore, SeedableRng, rngs::StdRng};
+use rand::{Rng, RngCore, SeedableRng, rngs::StdRng};
 use std::{
     fs::File,
     io::Write,
     path::{Path, PathBuf},
 };
+use uuid::Uuid;
+
+const ALIGN: u64 = 1024 * 4;
+
+pub fn generate_random_file_offset(
+    file_size: u64,
+    block_size: u64,
+    num_offsets: usize,
+    seed: u64,
+) -> Vec<u64> {
+    let last = file_size - block_size;
+    let mut r = StdRng::seed_from_u64(seed);
+
+    let offset_candidates: Vec<_> = (0..=last).step_by(ALIGN as usize).collect();
+
+    (0..num_offsets)
+        .map(|_| r.random_range(0..offset_candidates.len()))
+        .map(|index| (index as u64) * ALIGN)
+        .collect()
+}
 
 pub fn create_random_file(dir: &Path, size: &str, seed: u64) -> anyhow::Result<PathBuf> {
-    const PAGE_SIZE: u64 = 1024 * 4;
     let mut r = StdRng::seed_from_u64(seed);
 
     let num_page = {
         let num_bytes = parse_size(size)?;
-        assert!(num_bytes % PAGE_SIZE == 0);
-        num_bytes / PAGE_SIZE
+        assert!(num_bytes % ALIGN == 0);
+        num_bytes / ALIGN
     };
-    let path = dir.join("hello");
+    let path = dir.join(Uuid::new_v4().to_string());
 
     // write data
     {
         let mut file = File::create(&path)?;
-        let mut buffer = [0; PAGE_SIZE as usize];
+        let mut buffer = [0; ALIGN as usize];
         for _ in 0..num_page {
             r.fill_bytes(&mut buffer);
             file.write_all(&buffer)?;
         }
-        println!()
     }
 
     Ok(path)
@@ -35,9 +53,21 @@ pub fn create_random_file(dir: &Path, size: &str, seed: u64) -> anyhow::Result<P
 mod tests {
     use std::{fs::File, os::unix::fs::MetadataExt};
 
+    use parse_size::parse_size;
     use tempfile::tempdir;
 
     use crate::create_random_file;
+    use crate::generate_random_file_offset;
+
+    #[test]
+    fn test_generate_random_offsets() -> anyhow::Result<()> {
+        let file_size = parse_size("128 KiB")?;
+
+        let offsets = generate_random_file_offset(file_size, parse_size("8 KiB")?, 5, 123456);
+        println!("offsets={:?}", offsets);
+
+        Ok(())
+    }
 
     #[test]
     fn test_create_random_file() -> anyhow::Result<()> {
